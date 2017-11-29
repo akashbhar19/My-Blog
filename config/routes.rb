b76b1c60cdb362340cd65313791e78e85587f23a
Akash <akashbhar19@gmail.com>
2017-11-29 03:32:53 +0000

    Fixed Authorization Error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'auth/:provider/callback', to: 'session#create'
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
   
